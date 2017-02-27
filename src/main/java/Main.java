5ab5c32226755df4c09d6df7b02000418e249f76
tineo <cesar@tineo.mobi>
2017-02-26 21:21:17 -0500

    inicial

---
+++ src/main/java/Main.java
@@ -0,0 +1,44 @@
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    //port(Integer.valueOf(System.getenv("PORT")));
+    port(8080);
+    staticFileLocation("/public");
+
+    get("/hello", (req, res) -> "Hello World");
+
+    post("/formulario", (req, res) -> req.queryParams("nombre2"));
+
+    get("/formulario", (request, response) -> {
+        //Map<String, Object> attributes = new HashMap<>();
+        //attributes.put("message", "Hello World!");
+
+        return new ModelAndView(null, "form.ftl");
+    }, new FreeMarkerEngine());
+
+    get("/", (request, response) -> {
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("message", "Hello World!");
+
+        return new ModelAndView(attributes, "index.ftl");
+    }, new FreeMarkerEngine());
+
+
+  }
+
+}
---
+++ src/main/resources/spark/template/freemarker/form.ftl
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Formulario</title>
+</head>
+<body>
+
+<form method="get">
+    <label for="codigo1">Codigo</label>
+    <input type="text" name="codigo1" />
+    <label for="nombre2">Nombre</label>
+    <input type="text" name="nombre2" />
+
+    <input type="submit" value="Buscar">
+    
+</form>
+
+</body>
+</html>
